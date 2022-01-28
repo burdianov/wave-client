d008f01b7d92d274949e060bf757d488461cbf6b
Lilian Burdianov <lilian.burdianov@gmail.com>
2022-01-29 00:37:01 +0400

    create useEthers custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import { useEffect, useState } from 'react';
-import { ethers } from 'ethers';
 
-import wavePortalArtifact from './utils/WavePortal.json';
+import useEthers from './hooks/useEthers';
 
 export default function App() {
   const [currentAccount, setCurrentAccount] = useState('');
-  const [waveCount, setWaveCount] = useState();
-  const [loading, setLoading] = useState<boolean>(false);
 
-  const contractAddress = import.meta.env.VITE_CONTRACT_ADDRESS;
-
-  const contractABI = wavePortalArtifact.abi;
+  const [waveCount, wave, loading] = useEthers();
 
   const checkIfWalletIsConnected = async () => {
     try {
@@ -51,41 +46,6 @@ export default function App() {
       console.log('Connected', accounts[0]);
       setCurrentAccount(accounts[0]);
     } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const wave = async () => {
-    try {
-      setLoading(true);
-      const { ethereum } = window;
-
-      if (ethereum) {
-        const provider = new ethers.providers.Web3Provider(ethereum);
-        const signer = provider.getSigner();
-        const wavePortalContract = new ethers.Contract(
-          contractAddress,
-          contractABI,
-          signer
-        );
-
-        let count = await wavePortalContract.getTotalWaves();
-        console.log('Retrieved total wave count ...', count.toNumber());
-
-        const waveTxn = await wavePortalContract.wave();
-        console.log('Mining...', waveTxn.hash);
-
-        await waveTxn.wait();
-        console.log('Mined -- ', waveTxn.hash);
-
-        count = await wavePortalContract.getTotalWaves();
-        setWaveCount(count.toNumber());
-      } else {
-        console.log("Ethereum object doesn't exist");
-      }
-      setLoading(false);
-    } catch (error) {
-      setLoading(false);
       console.log(error);
     }
   };
